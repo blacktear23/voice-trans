e2afda9a27e423988c5b2749203c293c30c77304
yulai.li <blacktear23@gmail.com>
2024-01-11 21:12:41 +0800

    update

--- prompt_templates.py
+++ prompt_templates.py
@@ -19,4 +19,9 @@ PROMPT_TPLS = [
         'name': 'Give English Suggestion',
         'prompt': '你是一个英语老师，请根据用词，语法评价一下以下句子是否优秀, 如果不优秀，请提供一些建议: {prompt}'
     },
+    {
+        'key': 'english-teacher',
+        'name': 'English Teacher',
+        'prompt': '你是一个英语老师，请根据下面的问题进行口语练习回答: {prompt}',
+    },
 ]
